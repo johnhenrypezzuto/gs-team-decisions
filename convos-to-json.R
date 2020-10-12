2a18f6a9edf21381e19a7ffd9c68ee710f39be56
John-Henry Pezzuto <johnhenrypezzuto@gmail.com>
2020-10-12 12:40:13 -0500

    create reverse gender df; bring on 3 consistent names formatting

--- convos-to-json.R
+++ convos-to-json.R
@@ -9,39 +9,47 @@ set.seed(42)
 
 n_names = 500
 
-names_df <- bind_rows(
-  babynames %>% 
-    filter(sex == "F",
-           year > 1980) %>%
-    group_by(name, sex) %>% 
-    summarise(prop = sum(prop)) %>% 
-    arrange(desc(prop)) %>% 
-    head(n_names) %>% 
-    ungroup() %>% 
-    transmute(female = 1,
-              name,
-              n = row_number()),
-    babynames %>% 
-    filter(sex == "M",
-           year > 1980) %>%
-    group_by(name, sex) %>% 
-    summarise(prop = sum(prop)) %>% 
-    arrange(desc(prop)) %>% 
-    head(n_names) %>% 
-    ungroup() %>% 
-    transmute(female = 0,
-              name,
-              n = row_number())
-)
+# master name assignment db based on membernum and gender 
+names_df <- tibble(pos = rep(seq(1,3), 3), 
+                   convo = c(rep("convo1", 3), rep("convo2", 3), rep("convo3", 3)),
+                   male_name= c("Michael",
+                                "Christopher",
+                                "Matthew",
+                                "Joshua",
+                                "Jacob",
+                                "Nicholas",
+                                "Andrew",
+                                "Daniel",
+                                "Tyler"), 
+                   female_name = c("Jessica",
+                                   "Ashley",
+                                   "Emily",
+                                   "Sarah",
+                                   "Samantha",
+                                   "Amanda",
+                                   "Brittany",
+                                   "Elizabeth",
+                                   "Megan")
+                   ) %>% 
+  pivot_longer(cols = male_name:female_name, names_to = "gender") %>% 
+  pivot_wider(id_cols = c(pos, gender), names_from = convo, values_from = value) %>% 
+  mutate(female = case_when(gender == "male_name" ~ 0, 
+                            gender == "female_name" ~ 1)) %>% 
+  rename(membernum = pos)
+
 convo <- read_dta("/Users/jhap/Dropbox (Harvard University)/katie/Consideration of Ideas/Data/Chat Data.dta")
 
-## if I use membernum here it will always be 1, 2, 3
 
+
+## if I use membernum here it will always be 1, 2, 3
 gender <- read_dta("/Users/jhap/Dropbox (Harvard University)/katie/Consideration of Ideas/Data/main_data_reshaped_kyra.dta") %>% 
-  distinct(unique_group, subject, female) %>% 
-  mutate(n = as.integer(runif(nrow(.), min = 1, max = n_names))) %>% 
-  left_join(names_df) %>% 
-  select(-n)
+  distinct(unique_group, subject, female, membernum) %>% 
+  left_join(names_df) %>%  # each membernum gets assigned 3 names for each convo
+  left_join(names_df %>% 
+              select(-gender) %>% 
+              mutate(female = 1 - female) %>%  # reverse gender and join again
+              rename_at(vars(starts_with("convo")), function(x){str_c(x, "_rev")})
+            )
 
 
 
@@ -50,13 +58,13 @@ topic_df <- tibble(question = c(3, 8, 5, 2, 4, 1, 7, 6),
 
 
 
-
+# 
 convo_clean <- 
   convo %>%
-    select(unique_group, subject, chatentry, genderknown, secondsintochat, question) %>% 
+    select(unique_group, subject, chatentry, secondsintochat, question) %>% 
+    left_join(select(gender, subject, unique_group, female, membernum)) %>% # add female /membernum column
     drop_na(chatentry, subject) %>% 
     group_by(unique_group) %>% 
-    left_join(gender) %>% # import names
     mutate(subject = dense_rank(subject)) %>%  # renumber Member 1:3
     left_join(topic_df) %>% # import topics
   arrange(topic, unique_group, secondsintochat)
@@ -66,7 +74,6 @@ convo_prep <-
   convo_clean %>%
   mutate(
     subject = glue("<b>Member {subject}: </b>"), # bold the subject thing
-    name = glue("<b>{name}: </b>"),
     chatentry = str_replace_all(chatentry, "\\\\q.*?\\\\q", # fix quote glitch
                                 function(m) paste0("<q>", gsub("\\\\q", "", m), "</q>")),
     chatentry = str_c(chatentry, "<br>")
@@ -74,21 +81,27 @@ convo_prep <-
 
 
 
-### create name df
+### create name df for convo mapping
+names_df
 convo_clean %>% 
   group_by(topic, unique_group) %>% 
   mutate(subject = str_c("Member ", subject)) %>% 
-  distinct(genderknown, female, subject, name) %>% 
-  select(subject, name, everything()) %>% 
+  distinct(unique_group, female, subject, membernum) %>% 
+  left_join(select(gender, unique_group, membernum, starts_with("convo"))) %>% 
+  select(subject, starts_with("convo"), everything()) %>% 
   summarise(subject = list(subject),
-            name = list(name),
-            genderknown = list(genderknown),
+            name1 = list(convo1),
+            name2 = list(convo2),
+            name3 = list(convo3),
+            name_rev1 = list(convo1_rev),
+            name_rev2 = list(convo2_rev),
+            name_rev3 = list(convo3_rev),
             female = list(female)) %>% 
   toJSON(.) %>% 
   write("convo/pseudonyms-subject-match.json")
 
 
-### export anon convos as json  
+### export anon convos as json, one group per row
 convo_prep %>% 
   group_by(topic, unique_group) %>% 
   mutate(chatentry = str_c(subject, chatentry)) %>% 
@@ -97,13 +110,35 @@ convo_prep %>%
   write("convo/chat-anon.json")
 
 
-### export gender convos as json  
+### export gender convos as json, one group per row
 convo_prep %>% 
+  left_join(select(gender, "unique_group", "membernum", convo1:convo3), 
+            by = c("unique_group", "membernum")) %>% # import names
   group_by(topic, unique_group) %>% 
-  mutate(chatentry = str_c(name, chatentry)) %>% 
-  summarise(chat = paste0(chatentry, collapse = "")) %>% 
+  mutate_at(vars(convo1:convo3), ~str_c("<b>", ., ": </b>")) %>% # format names
+  mutate(chatentry1 = str_c(convo1, chatentry), # add names to convos
+         chatentry2 = str_c(convo2, chatentry),
+         chatentry3 = str_c(convo3, chatentry)) %>% 
+  summarise(chat1 = paste0(chatentry1, collapse = ""), # collapse convos
+            chat2 = paste0(chatentry2, collapse = ""),
+            chat3 = paste0(chatentry3, collapse = "")) %>% 
   toJSON(.) %>% 
   write("convo/chat-gender.json")
+
+### export reversed gender convos as json  
+convo_prep %>% 
+  left_join(select(gender, "unique_group", "membernum", convo1_rev:convo3_rev), # we import the reversed names
+            by = c("unique_group", "membernum")) %>% # import names
+  group_by(topic, unique_group) %>% 
+  mutate_at(vars(convo1_rev:convo3_rev), ~str_c("<b>", ., ": </b>")) %>% 
+  mutate(chatentry1 = str_c(convo1_rev, chatentry), 
+         chatentry2 = str_c(convo2_rev, chatentry),
+         chatentry3 = str_c(convo3_rev, chatentry)) %>% 
+  summarise(chat1 = paste0(chatentry1, collapse = ""),
+            chat2 = paste0(chatentry2, collapse = ""),
+            chat3 = paste0(chatentry3, collapse = "")) %>% 
+  toJSON(.) %>% 
+  write("convo/chat-gender-rev.json")
 
 
 
